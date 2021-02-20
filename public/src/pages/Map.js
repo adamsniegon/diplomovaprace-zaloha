3eab11f25a0ba6e6d7b66e6793ca00de9f7a976c
Adam <a.sniegon@seznam.cz>
2021-02-20 02:05:28 +0100

    feat: Add basic map

--- public/src/pages/Map.js
+++ public/src/pages/Map.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 function Map() {
     return (
-        <React.Fragment>
-            <h1>Map!</h1>
-        </React.Fragment>
+        <MapContainer style={{width: "100%", height: "100vh"}} center={[49.844891, 18.429800]} zoom={12}>
+            <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        </MapContainer>
     );
   }
   
